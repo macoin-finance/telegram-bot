8239a07478aa2e3fcf416335ac99a728d670e9a2
KnZ <qnsx@protonmail.com>
2021-07-11 19:54:07 -0300

    Changed video to iphone. They had the same url redirect (android video).

--- commands/video.py
+++ commands/video.py
@@ -1,4 +1,4 @@
-MESSAGE_TEXT='**Android:** https://macoin.finance/android\n**iPhone:** https://macoin.finance/video'
+MESSAGE_TEXT='**Android:** https://macoin.finance/android\n**iPhone:** https://macoin.finance/iphone'
 
 def video(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=MESSAGE_TEXT , parse_mode='markdown', disable_web_page_preview='True')
