7a3a234b765ff53e91b147f59dbf1d219a50e92f
KnZ <qnsx@protonmail.com>
2021-07-11 20:29:58 -0300

    data mudada com sucesso

--- commands/launch.py
+++ commands/launch.py
@@ -1,4 +1,4 @@
-MESSAGE_TEXT='**Data do lançamento oficial:** 17-07-2021\n**Dias restantes para o lançamento:** 7'
+MESSAGE_TEXT='**Data do lançamento oficial:** 17-07-2021\n**Dias restantes para o lançamento:** 6'
 
 def launch(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=MESSAGE_TEXT , parse_mode='markdown')
