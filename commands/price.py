aec390e0aa600da9c3981fb2b661281d3d382cb1
macoin-finance-dev <developer@macoin.finance>
2021-07-12 08:56:48 -0300

    clean code

--- commands/price.py
+++ commands/price.py
@@ -6,42 +6,42 @@ from datetime import datetime
 
 
 try:
-    MACOIN = requests.get('https://api.pancakeswap.info/api/v2/tokens/0x20F23bC6F28bd31f9869b9C7750fDEaFED7d22Cd').json()["data"]
-    TIMESTAMP = requests.get('https://api.pancakeswap.info/api/v2/tokens/0x20F23bC6F28bd31f9869b9C7750fDEaFED7d22Cd').json()['updated_at']
-    HUMANTIME = datetime.fromtimestamp(int(TIMESTAMP/1000))
-    USDBRL = requests.get('https://economia.awesomeapi.com.br/all/USD').json()['USD']['high']
-    MACOINUSD = requests.get('https://api.pancakeswap.info/api/v2/tokens/0x20F23bC6F28bd31f9869b9C7750fDEaFED7d22Cd').json()["data"]['price']
 
+    pancakeswap = requests.get('https://api.pancakeswap.info/api/v2/tokens/0x20F23bC6F28bd31f9869b9C7750fDEaFED7d22Cd').json()
+    macoin = pancakeswap.get('data')
+    timestamp = pancakeswap.get('updated_at')
+    humantime = datetime.fromtimestamp(int(timestamp/1000))
+    usd_brl = requests.get('https://economia.awesomeapi.com.br/all/USD').json()['USD']['high']
+    macoin_usd = pancakeswap.get('data', {}).get('price')
+    macoin_bnb = pancakeswap.get('data', {}).get('price_BNB')
 
-    valuebrl = float(USDBRL)
-    valueusd = float(MACOINUSD)
+    valuebrl = float(usd_brl)
+    valueusd = float(macoin_usd)
     macoinbrlresult = (valuebrl * valueusd)
-    MACOINBRL = (round(macoinbrlresult, 20))
+    macoin_brl = (round(macoinbrlresult, 20))
 
-    MESSAGE_TEXT = f"**MaCoin price USD:** ${MACOIN['price']}\n**MaCoin price BNB:** ${MACOIN['price_BNB']}\n**MaCoin price BRL:** R${MACOINBRL}\n\nPreço atualizado em: {HUMANTIME}"
-
+    MESSAGE_TEXT = f"**MaCoin price USD:** ${macoin_usd}\n**MaCoin price BNB:** ${macoin_bnb}\n**MaCoin price BRL:** R${macoin_brl}\n\nPreço atualizado em: {humantime}"
     def price(update, context):
         context.bot.send_message(chat_id=update.effective_chat.id, text=MESSAGE_TEXT, parse_mode='markdown')
 
 except requests.exceptions.RequestException as e:
-     try:
+    try:
+        org = requests.get('https://bsc.api.0x.org/swap/v1/price?sellToken=0x20F23bC6F28bd31f9869b9C7750fDEaFED7d22Cd&buyToken=BUSD&sellAmount=1').json()
+        orgbnb = requests.get('https://bsc.api.0x.org/swap/v1/price?sellToken=0x20F23bC6F28bd31f9869b9C7750fDEaFED7d22Cd&buyToken=BNB&sellAmount=1').json()
+        macoin_usd = org.get('price')
+        macoin_bnb = orgbnb.get('price')
+        usd_brl = requests.get('https://economia.awesomeapi.com.br/all/USD').json()['USD']['high']
 
-         USDBRL = requests.get('https://economia.awesomeapi.com.br/all/USD').json()['USD']['high']
-         MACOINUSD = requests.get('https://bsc.api.0x.org/swap/v1/price?sellToken=0x20F23bC6F28bd31f9869b9C7750fDEaFED7d22Cd&buyToken=BUSD&sellAmount=1').json()
-         MACOINBUSD = requests.get('https://bsc.api.0x.org/swap/v1/price?sellToken=0x20F23bC6F28bd31f9869b9C7750fDEaFED7d22Cd&buyToken=BUSD&sellAmount=1').json()
-         MACOINBNB = requests.get('https://bsc.api.0x.org/swap/v1/price?sellToken=0x20F23bC6F28bd31f9869b9C7750fDEaFED7d22Cd&buyToken=BNB&sellAmount=1').json()
- 
-         valuebrl = float(USDBRL)
-         valueusd = float(MACOINBUSD)
-         macoinbrlresult = (valuebrl * valueusd)
-         MACOINBRL = (round(macoinbrlresult, 20))
- 
-         MESSAGE_TEXT0x = f"**MaCoin price USD:** ${MACOINUSD['price']}\n**MaCoin price BNB:** ${MACOINBNB['price']}\n**MaCoin price BRL:** R${MACOINBRL}"
- 
-         def price(update, context):
-             context.bot.send_message(chat_id=update.effective_chat.id, text=MESSAGE_TEXT0x, parse_mode='markdown')
- 
-     except requests.exceptions.RequestException as e:
+        valuebrl = float(usd_brl)
+        valueusd = float(macoin_usd)
+        macoinbrlresult = (valuebrl * valueusd)
+        macoin_brl = (round(macoinbrlresult, 20))
+
+        MESSAGE_TEXT0x = f"**MaCoin price USD:** ${macoin_usd}\n**MaCoin price BNB:** ${macoin_bnb}\n**MaCoin price BRL:** R${macoin_brl}"
+        def price(update, context):
+            context.bot.send_message(chat_id=update.effective_chat.id, text=MESSAGE_TEXT, parse_mode='markdown')
+
+    except requests.exceptions.RequestException as e:
  
              MESSAGE_TEXTOFF = f"**API da Pancakeswap.info e bsc.api.0x.org fora do ar. Tente novamente em alguns minutos."
  
