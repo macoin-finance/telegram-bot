1c6246754f49ba48be11d9b75af1caee975180aa
macoin-finance-dev <developer@macoin.finance>
2021-07-12 01:00:04 -0300

    new price

--- commands/price.py
+++ commands/price.py
@@ -1,8 +1,14 @@
 import requests
+from datetime import datetime
+
+
 
 MACOIN = requests.get('https://api.pancakeswap.info/api/v2/tokens/0x20F23bC6F28bd31f9869b9C7750fDEaFED7d22Cd').json()["data"]
+TIMESTAMP = requests.get('https://api.pancakeswap.info/api/v2/tokens/0x20F23bC6F28bd31f9869b9C7750fDEaFED7d22Cd').json()['updated_at']
 
-MESSAGE_TEXT = f"**MaCoin price USD:** ${MACOIN['price']}\n**MaCoin price BNB:** ${MACOIN['price_BNB']}"
+HUMANTIME = datetime.fromtimestamp(TIMESTAMP/1000)
+
+MESSAGE_TEXT = f"**MaCoin price USD:** ${MACOIN['price']}\n**MaCoin price BNB:** ${MACOIN['price_BNB']}\n\nPreço atualizado em: {HUMANTIME}"
 
 def price(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=MESSAGE_TEXT, parse_mode='markdown')
