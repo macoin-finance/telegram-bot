202ff99b1a012e3c2ea7ccb60e9840830d1cb149
macoin-finance-dev <developer@macoin.finance>
2021-07-23 08:29:30 -0300

    new exchange - app.xchange.ee, disabled old exchange - SECURITY REASONS - Returns to exchange PancakeSwap

--- commands/buy.py
+++ commands/buy.py
@@ -1,4 +1,4 @@
-MESSAGE_TEXT='**Buy using BNB:**\thttps://macoin.finance/buy\n\n**Buy using PIX (BRL):**\thttps://macoin.finance/pix'
+MESSAGE_TEXT='**Buy using PancakeSwap:**\thttps://macoin.finance/buy\n\n**Buy using PIX (Brazil)  or credit card:**\thttps://macoin.finance/xchange'
 
 def buy(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=MESSAGE_TEXT , parse_mode='markdown', disable_web_page_preview='True')
--- commands/news.py
+++ commands/news.py
@@ -1,4 +1,4 @@
-MESSAGE_TEXT='**Latest news**:\n\nhttps://www.lacryptomonnaie.net/portugais/macoin-est-le-nouveau-token-meme-cree-au-bresil/\n\nhttps://criptoeconomia.com.br/macoin-e-o-novo-token-meme-criado-no-brasil/\n\nhttps://www.reddit.com/r/farialimabets/comments/oat7b6/macoin_beck_token_100_brasileiro_focado_em/\n\nCoin Sniper:\thttps://coinsniper.net/coin/7600\n\n\n\nAccess our channel for news about MaCoin: https://t.me/macoinfinance'
+MESSAGE_TEXT='**Latest news**:\n\nhttps://www.lacryptomonnaie.net/portugais/macoin-est-le-nouveau-token-meme-cree-au-bresil/\n\nhttps://criptoeconomia.com.br/macoin-e-o-novo-token-meme-criado-no-brasil/\n\nhttps://www.reddit.com/r/farialimabets/comments/oat7b6/macoin_beck_token_100_brasileiro_focado_em/\n\nCoin Gecko:\thttps://www.coingecko.com/en/coins/macoin\n\nDetail Review:\thttps://www.youtube.com/watch?v=UKCalIK5D90\n\nCoin Sniper:\thttps://coinsniper.net/coin/7600\n\n\n\nAccess our channel for news about MaCoin: https://t.me/macoinfinance'
 
 def news(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=MESSAGE_TEXT , parse_mode='markdown', disable_web_page_preview='True')
