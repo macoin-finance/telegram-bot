24ce613510a5c45872298b0098fdd544fe797826
macoin-finance-dev <developer@macoin.com.br>
2021-09-01 00:11:09 -0300

    PAPER V0 OFICIAL MACOIN COMUNIDADE - A REAL HISTORIA DA MACOIN

--- commands/paper.py
+++ commands/paper.py
@@ -1,4 +1,4 @@
-MESSAGE_TEXT='**GreenPaper:**\thttps://github.com/macoin.com.br/macoin-site/blob/main/paper/MaCoin-GreenPaper-v1.pdf'
+MESSAGE_TEXT='**GreenPaper:**\thttps://github.com/macoin.com.br/macoin-site/blob/main/paper/MaCoin-GreenPaper-v0.pdf'
 
 def paper(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=MESSAGE_TEXT , parse_mode='markdown', disable_web_page_preview='True')
